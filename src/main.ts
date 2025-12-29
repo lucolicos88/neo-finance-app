56dc0ccb12ad408f4144162eb4049b57f4b5b838
tineoformula <lucolicos@gmail.com>
2025-12-29 15:37:52 -0300

    Fit caixa report to single page

--- src/main.ts
+++ src/main.ts
@@ -82,7 +82,7 @@ import {
   clearCaches,
 } from './services/webapp-service';
 
-const DEPLOY_LABEL = 'v125 - datas e logo caixa';
+const DEPLOY_LABEL = 'v126 - imprimir caixa 1 pagina';
 
 function isDebugApiEnabled(): boolean {
   return PropertiesService.getScriptProperties().getProperty('ENABLE_DEBUG_API') === 'true';
